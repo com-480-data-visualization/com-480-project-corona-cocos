032602a3c1244e635d125d53ce473f86c5ba2493
Lucien Iseli <lucien.iseli93@gmail.com>
2020-04-09 10:09:33 +0200

    Start implementation of zoom, doesn't work

--- visualization.js
+++ visualization.js
@@ -1,6 +1,6 @@
 var margin = { top:50, left: 50, right:50, bottom:50},
-height = 400 - margin.top - margin.bottom,
-width = 800 - margin.left - margin.right;
+height = 1000 - margin.top - margin.bottom,
+width = 1900 - margin.left - margin.right;
 console.log(height);
 console.log(width);
 
@@ -23,12 +23,30 @@ var projection = d3.geoMercator()
 var path = d3.geoPath()
     .projection(projection);
 
+var countries;
+
 function ready(error, data) {
     console.log('ready')
 
-    var countries = topojson.feature(data, data.objects.countries).features;
+    countries = topojson.feature(data, data.objects.countries).features;
 
     console.log(countries);
+
+    svg.selectAll(".country")
+        .data(countries)
+        .enter().append("path")
+        .attr("class", "country")
+        .attr("d", path)
+}
+
+function europeZoom() {
+    console.log("Europe zoom")
+    projection = d3.geoMercator()
+        .translate([width / 2, height / 2 + 50])
+        .scale(400)
+
+    path = d3.geoPath()
+        .projection(projection);
 
     svg.selectAll(".country")
         .data(countries)
