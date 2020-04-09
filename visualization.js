fa7f6bf6218cb496d8cf2518ba1275990702f9e6
Jules Gottraux <jules.gottraux@epfl.ch>
2020-04-09 17:42:19 +0200

    Add zoom button and function

--- visualization.js
+++ visualization.js
@@ -1,6 +1,6 @@
 var margin = { top:50, left: 50, right:50, bottom:50},
-height = 1000 - margin.top - margin.bottom,
-width = 1900 - margin.left - margin.right;
+height = 600 - margin.top - margin.bottom,
+width = 1200 - margin.left - margin.right;
 console.log(height);
 console.log(width);
 
@@ -10,14 +10,14 @@ var svg = d3.select("#map")
 .attr("height", height + margin.top + margin.bottom)
 .attr("width", width + margin.left + margin.right)
 .append("g")
-.attr("transform", "translate("+margin.left+","+margin.top+")");
+.attr("transform", "translate("+margin.left+","+margin.top+")")
+.on('click', continentZoom);
 
 d3.queue()
 .defer(d3.json, "world.topojson")
 .await(ready)
 
 var projection = d3.geoMercator()
-    .translate([width / 2, height / 2 + 50])
     .scale(100);
 
 var path = d3.geoPath()
@@ -37,20 +37,61 @@ function ready(error, data) {
         .enter().append("path")
         .attr("class", "country")
         .attr("d", path)
+		continentZoom('worldButton')
 }
 
-function europeZoom() {
-    console.log("Europe zoom")
-    projection = d3.geoMercator()
-        .translate([width / 2, height / 2 + 50])
-        .scale(400)
+function continentZoom(idButton) {
+	let x
+	let y
+	let zoom
 
-    path = d3.geoPath()
-        .projection(projection);
+	if (idButton == 'worldButton') {
+		zoom = 2
+		x = width/20
+		y = height/10
+	} else if(idButton == 'euButton') {
+		zoom = 6
+		x = width/20
+		y = height/4
+	} else if(idButton == 'asiaButton') {
+		x = -width/10
+		y = height/6
+		zoom = 3
+	} else if(idButton == 'naButton') {
+		x = width/4.5
+		y = height/5
+		zoom = 4.5
+	} else if(idButton == 'saButton') {
+		x = width/7
+		y = -height/18
+		zoom = 4.2
+	} else if(idButton == 'afrButton') {
+		x = width/20
+		y = height/20
+		zoom = 3.8
+	} else if(idButton == 'austrButton') {
+		x = -width/7
+		y = -height/15
+		zoom = 3.8
+	}
 
-    svg.selectAll(".country")
-        .data(countries)
-        .enter().append("path")
-        .attr("class", "country")
-        .attr("d", path)
+	console.log(x+" "+width)
+	console.log(y+" "+height)
+
+	center_x = (width/2)-(width/(zoom*2))
+	center_y = height/2-height/(zoom*2)
+
+	console.log("cx"+center_x+" cy"+center_y)
+
+	x = -center_x + x
+	y = -center_y + y
+
+	console.log('x final '+x+" y final "+y)
+
+	let s = `scale(${zoom})`+`translate(${x},${y})`
+	console.log("str: "+s)
+
+	svg.transition()
+		.duration(1000)
+		.attr('transform', s)
 }
