37e92621cad25224669e673a02210cf8322d7322
Espremea <contact.nicolaszimmermann@gmail.com>
2020-04-22 11:12:43 +0200

    working on graphs

--- visualization.js
+++ visualization.js
@@ -1,3 +1,6 @@
+const minDate = "1/22/20";
+const maxDate = "4/8/20";
+
 const margin = {top: 50, left: 50, right: 50, bottom: 50},
 	height = 600 - margin.top - margin.bottom,
 	width = 1200 - margin.left - margin.right;
@@ -225,7 +228,7 @@ function timeSlider(svg, path, countries, dataset, startingDate) {
  * @param date
  */
 function displayCountries(svg, path, countries, dataset, date) {
-    const max = d3.max(dataset, d => parseInt(d['4/8/20']) / parseInt(d['population']) * 1_000_000 + 1);
+    const max = d3.max(dataset, d => parseInt(d[maxDate]) / parseInt(d['population']) * 1_000_000 + 1);
     const logMax = Math.log2(max)
 
     var colorScale = d3.scaleLinear()
@@ -233,7 +236,7 @@ function displayCountries(svg, path, countries, dataset, date) {
         .range(['#FFFFFF', '#FF0000', '#000000']);
 
     // append a defs (for definition) element to your SVG
-    var svgLegend = d3.select('body').append('svg')
+    var svgLegend = d3.select("#legend").append('svg')
         .attr("width", 900);
     var defs = svgLegend.append('defs');
 
@@ -308,7 +311,7 @@ function displayCountries(svg, path, countries, dataset, date) {
 }
 
 function updateCountriesColor(svg, path, countries, dataset, date) {
-    const max = d3.max(dataset, d => parseInt(d['4/8/20']) / parseInt(d['population']) * 1_000_000 + 1);
+    const max = d3.max(dataset, d => parseInt(d[maxDate]) / parseInt(d['population']) * 1_000_000 + 1);
     const logMax = Math.log2(max)
 
     svg.selectAll(".country")
@@ -322,17 +325,128 @@ function updateCountriesColor(svg, path, countries, dataset, date) {
                 return hslToHex(180, 1, 50)
             }
         })
-        .on("click", (d, _) => {
-            const name = d.properties.name
-            const match = dataset.filter(row => row['Country/Region'] === name)
-            if (match.length > 0) {
-                const infectedRate = parseInt(match[0][date]) / parseInt(match[0]['population'])
+        .on("click", d => onCountryClicked(d, dataset, date))
+}
 
-                console.log(name)
-                console.log(`${infectedRate * 100}% of population infected`)
-                console.log(`${parseInt(match[0][date]) } people infected`)
-            }
-        })
+function onCountryClicked(countryData, dataset, date) {
+    const name = countryData.properties.name
+    const match = dataset.filter(row => row['Country/Region'] === name)
+    if (match.length > 0) {
+        const infectedRate = parseInt(match[0][date]) / parseInt(match[0]['population'])
+
+        console.log(name)
+        console.log(`${infectedRate * 100}% of population infected`)
+        console.log(`${parseInt(match[0][date]) } people infected`)
+    }
+
+    console.log(match);
+
+
+    // set the dimensions and margins of the graph
+    var margin = {top: 10, right: 30, bottom: 30, left: 60},
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#graph")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    // Read the data
+    d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_IC.csv",function(data) {
+
+        // Add X axis --> it is a date format
+        var x = d3.scaleTime()
+            .domain([minDate,maxDate])
+            .range([ 0, width ]);
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+        // Add Y axis
+        var y = d3.scaleLinear()
+            .domain([0, 13])
+            .range([ height, 0 ]);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        // This allows to find the closest X index of the mouse:
+        var bisect = d3.bisector(function(d) { return d.x; }).left;
+
+        // Create the circle that travels along the curve of chart
+        var focus = svg
+            .append('g')
+            .append('circle')
+            .style("fill", "none")
+            .attr("stroke", "black")
+            .attr('r', 8.5)
+            .style("opacity", 0)
+
+        // Create the text that travels along the curve of chart
+        var focusText = svg
+            .append('g')
+            .append('text')
+            .style("opacity", 0)
+            .attr("text-anchor", "left")
+            .attr("alignment-baseline", "middle")
+
+        // Create a rect on top of the svg area: this rectangle recovers mouse position
+        svg
+            .append('rect')
+            .style("fill", "none")
+            .style("pointer-events", "all")
+            .attr('width', width)
+            .attr('height', height)
+            //.on('mouseover', mouseover)
+            //.on('mousemove', mousemove)
+            //.on('mouseout', mouseout);
+
+        // Add the line
+        svg
+            .append("path")
+            .datum(dataset)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 1.5)
+            .attr("d", d3.line()
+                .x(function(d) {
+                    return x(d.x)
+                })
+                .y(function(d) {
+                    return y(d.y)
+                })
+            )
+
+
+        // What happens when the mouse move -> show the annotations at the right positions.
+        function mouseover() {
+            focus.style("opacity", 1)
+            focusText.style("opacity",1)
+        }
+
+        function mousemove() {
+            // recover coordinate we need
+            var x0 = x.invert(d3.mouse(this)[0]);
+            var i = bisect(data, x0, 1);
+            selectedData = data[i]
+            focus
+                .attr("cx", x(selectedData.x))
+                .attr("cy", y(selectedData.y))
+            focusText
+                .html("x:" + selectedData.x + "  -  " + "y:" + selectedData.y)
+                .attr("x", x(selectedData.x)+15)
+                .attr("y", y(selectedData.y))
+        }
+        function mouseout() {
+            focus.style("opacity", 0)
+            focusText.style("opacity", 0)
+        }
+
+    })
 }
 
 d3.csv('/generated/confirmed.csv', dataset => {
@@ -361,8 +475,8 @@ d3.csv('/generated/confirmed.csv', dataset => {
         console.log(countries);
 
 
-        displayCountries(svg, path, countries, dataset, '1/22/20')
-        timeSlider(svg, path, countries, dataset, '1/22/20')
+        displayCountries(svg, path, countries, dataset, minDate)
+        timeSlider(svg, path, countries, dataset, minDate)
         continentZoom('worldButton')
     }
 })
